b072cc4dc49aa25f740a8008cc64cbb9e5ab0f22
Carlos Daniel Costa de Sousa <101117494+CarlDevx@users.noreply.github.com>
2024-06-09 03:36:53 +0000

    main: first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+class Hero{
+    constructor(name, victories, defeats){
+        this.name = name;
+        this.victories = victories;
+        this.defeats = defeats;
+    }
+}
+
+class Match{
+
+}
